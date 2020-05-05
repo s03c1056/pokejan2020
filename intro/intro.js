71f3bc84472bf1fbfb93fb8174d49d949a3f2999
s03c1056 <s03c1056@gmail.com>
2020-05-06 01:10:21 +0900

    modified:   intro/intro.js

--- intro/intro.js
+++ intro/intro.js
@@ -1,26 +1,26 @@
-onload = function() {
+onload = function () {
   start();
 };
 function sleep(msec) {
   /////////////////////sleep関数をつくった
-  return new Promise(function(resolve) {
+  return new Promise(function (resolve) {
     ///await sleep(5000);
-    setTimeout(function() {
+    setTimeout(function () {
       /////////////////↑こう書けば5秒処理を待機
       resolve();
     }, msec);
   });
 }
 async function start() {
-  document.onkeydown = function() {
+  document.onkeydown = function () {
     location.href = "../demo/demo.html";
   };
-  document.ontouchstart = function() {
+  document.ontouchstart = function () {
     location.href = "../demo/demo.html";
   };
   document.getElementById("textBox").style.display = "none";
   await sleep(3000);
   document.getElementById("textBox").style.display = "block";
   document.getElementById("text").innerHTML =
-    "あ、やせいのピカチュウがとびだしてきた！▼";
+    "▼！たきてしだびとがウュチカピのいせや、あ";
 }
